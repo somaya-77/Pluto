1a20baff50fd82ce00d610c120ea8a37505c75f8
somaya-77 <96093076+somaya-77@users.noreply.github.com>
2023-02-02 20:59:40 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+let siaderbtn = document.querySelector(".fa-bars");
+let siader = document.querySelector(".menu-main");
+
+siaderbtn.addEventListener("click", ()=> {
+
+    siader.classList.toggle("close");
+    
+})
